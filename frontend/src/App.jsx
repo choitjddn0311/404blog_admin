c124e318bc56bb945f21b6f2017f8ba4dad49517
choitjddn0311 <cswcjw311427@naver.com>
2025-07-07 18:02:53 +0900

    feat: update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,22 @@ import MainDashboard from "./page/mainDashboard";
 import UserManagement from "./page/userManagement";
 import PostManagement from "./page/postManageMent";
 import DetailPost from "./page/detailPost";
+import UpdateLog from "./page/updateLog"
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <>
     <GlobalStyle/>
       <BrowserRouter>
-        <SideBar/>
+        <SideBar isOpen={isOpen} setIsOpen={setIsOpen}/>
         <Routes>
-          <Route path="/" element={<MainDashboard/>}></Route>
-          <Route path="/userList" element={<UserManagement/>} ></Route>
-          <Route path="/postList" element={<PostManagement/>} ></Route>
+          <Route path="/" element={<MainDashboard isOpen={isOpen}/>}></Route>
+          <Route path="/userList" element={<UserManagement isOpen={isOpen}/>} ></Route>
+          <Route path="/postList" element={<PostManagement isOpen={isOpen}/>} ></Route>
           <Route path="/postList/:id" element={<DetailPost/>}></Route>
+          <Route path="/UpdateLog" element={<UpdateLog/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
