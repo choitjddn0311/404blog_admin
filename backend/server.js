934c62e1283dce36acf48011b25fc569f102fe04
choitjddn0311 <cswcjw311427@naver.com>
2025-07-07 15:09:26 +0900

    feat: 백엔드 라우터 추가

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5001;
+const userManagementRoute = require('./routes/userManagement');
+const postManagementRoute = require('./routes/postManagement');
 
-app.get('/' , (req,res) => {
-    res.send("hello node.js backend")
-})
+
+// app.get('/' , (req,res) => {
+//     res.send("hello node.js backend")
+// })
+
+app.use(cors({
+  origin: `${process.env.REACT_APP_FRONT_URL}`,
+  methods: ['GET', 'POST' , 'PUT' , 'DELETE'],
+  credentials: true
+}));
+app.use(express.json());
+app.use('/admin' , userManagementRoute);
+app.use('/admin' , postManagementRoute);
 
 app.listen(port , '0.0.0.0' , () => {
     console.log(`${port}포트에서 돌아가는중`)
